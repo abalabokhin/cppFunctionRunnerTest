f55ad1e621ecc830017a3d6a7a234aa81db5d441
Andrey Balabokhin <abalabokhin@gmail.com>
2014-09-09 14:54:21 -0400

    some changes

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ public:
 
     template<typename... Values> void Run(RunningSettings rs, Values... values) {
         for (int i = 0; i < rs.maxI; ++i) {
-            function(values..., RunningSettings{i, 0});
+            function(RunningSettings{i, 0}, values...);
         }
     }
 
@@ -48,33 +48,52 @@ private:
     }
 
     template<typename ArgsT, typename FuncArgsT, typename... ArgsTail, typename... FuncArgsTail>
-    std::function<void(FuncArgsTail... funcTail, RunningSettings)> AddArgs(std::function<void(FuncArgsT, FuncArgsTail... funcTail, RunningSettings)> fn, ArgsT head, ArgsTail... tail) {
+    std::function<void(RunningSettings)> AddArgs(std::function<void(RunningSettings, FuncArgsT, FuncArgsTail... funcTail)> fn, ArgsT head, ArgsTail... tail) {
         return AddArgs(SetArg(fn, head), tail...);
     }
 
     template <typename ArgsT, typename FuncArgsT, typename... Tail>
-    std::function<void(Tail... tail, RunningSettings)> SetArg(std::function<void(FuncArgsT, Tail... tail, RunningSettings)> fn, ArgsT value) {
-        return std::bind(fn, value, std::placeholders::_1);
+    std::function<void(RunningSettings, Tail... tail)> SetArg(std::function<void(RunningSettings, FuncArgsT, Tail... tail)> fn, ArgsT value) {
+        if (sizeof...(Tail) == 0)
+            return std::bind(fn, std::placeholders::_1, value);
+        //if (sizeof...(Tail) == 1)
+//            return std::bind(fn, std::placeholders::_1, value, std::placeholders::_3);
+//        if (sizeof...(Tail) == 2)
+//            return std::bind(fn, std::placeholders::_1, value);
     }
 
     Fn * function;
 };
 
-void funcA(int a, int b/*, std::string const & c*/, RunningSettings i) {
+void funcA(RunningSettings i, int a, int b/*, std::string const & c*/) {
     std::cout << "funcA " << a + b << std::endl;
 }
 
-void funcB(int a, RunningSettings i) {
+void funcB(RunningSettings i, int a) {
     std::cout << "funcB " << a << std::endl;
 }
+
+void funcC(int i, int a) {
+
+}
+
+/*std::function<void(RunningSettings i, int a)> Bind() {
+    return std::bind(funcA, std::placeholders::_1, std::placeholders::_2, 12);
+
+}*/
 
 int main() {
     FunctionRunner<decltype(funcA)> runnerA(funcA);
     runnerA.Run(RunningSettings{3, 1}, 3, 4);
 
-    RSFunctionRunner<decltype(funcA)> runnerB_(funcA);
+//    RSFunctionRunner<decltype(funcA)> runnerA_(funcA);
     int a = 6;
-    runnerB_.Run(RunningSettings{2, 1}, a, 34);
+//    runnerA_.Run(RunningSettings{2, 1}, a, 4);
+
+    RSFunctionRunner<decltype(funcB)> runnerB_(funcB);
+    runnerB_.Run(RunningSettings{2, 1}, a);
+
+
 
     return 0;
 }
